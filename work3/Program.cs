28a570b385a33bb9df5a6800d208cff73d5b2acb
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2022-12-06 15:22:00 +0300

    Добавил домашние работы по четвертому семинару

---
+++ work3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача №29. Программа, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку
+
+int[] array = new int[8];
+Random myRandom = new Random();
+Console.WriteLine("Вывод массива");
+for (int i = 0; i < array.Length; i++) array[i] = myRandom.Next(0, 8);
+for (int i = 0; i < array.Length; i++) Console.Write("{0} ", array[i]);
+
